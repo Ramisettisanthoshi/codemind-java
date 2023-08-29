a4c07ebc878b65f940d1e01fa68f095d82eafd74
Ramisettisanthoshi <143481554+Ramisettisanthoshi@users.noreply.github.com>
2023-08-29 12:05:03 +0530

    import java.util.*; public class hii {     public static void main(String args[])     {         int a,b;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         int c=a*b;        System.out.println(c);     } }

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class hii
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        int c=a*b;
+       System.out.println(c);
+    }
+}
