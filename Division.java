866b6cafdcac18d0d5739d321ddef09fb8b3a566
Ramisettisanthoshi <143481554+Ramisettisanthoshi@users.noreply.github.com>
2023-09-01 20:49:37 +0530

    import java.util.*; public class hii {     public static void main(String[] args)     {         int a,b;         int d;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         d=a/b;         System.out.printf("%d",d);
        }
    }

---
+++ Division.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class hii
+{
+    public static void main(String[] args)
+    {
+        int a,b;
+        int d;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        d=a/b;
+        System.out.printf("%d",d);
+        
+    }
+}
