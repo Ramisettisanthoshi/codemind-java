fbaafb49d82151090a84fa45fb8c91939e941104
Ramisettisanthoshi <143481554+Ramisettisanthoshi@users.noreply.github.com>
2023-09-01 20:43:19 +0530

    import java.util.*; public class hii {     public static void main(String args[])     {         int a,b;         double d;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt(); d=(a+b)/2.0; System.out.printf("%.4f",d);
        }
    }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class hii
+{
+    public static void main(String args[])
+    {
+        int a,b;
+        double d;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+d=(a+b)/2.0;
+System.out.printf("%.4f",d);
+        
+    }
+}
