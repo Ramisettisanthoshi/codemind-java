2bb853a3e49dbc6b9721d041ed6099a090002e61
Ramisettisanthoshi <143481554+Ramisettisanthoshi@users.noreply.github.com>
2023-09-02 10:11:52 +0530

    import java.util.*; public class hii
    {
        public static void main(String args[])
        {
            int n1,n2;
            Scanner sc=new Scanner(System.in);
            n1=sc.nextInt();
            n2=sc.nextInt();
            if(n1>n2)
            {
                System.out.println(n1);
            }
            else
            {
                System.out.println(n2);
            }
    }
    }

---
+++ Biggest_of_Two_Numbers.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class hii
+
+{
+    public static void main(String args[])
+    {
+        int n1,n2;
+        Scanner sc=new Scanner(System.in);
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+        if(n1>n2)
+        {
+            System.out.println(n1);
+        }
+        else
+        {
+            System.out.println(n2);
+        }
+}
+}
