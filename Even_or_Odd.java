7f63df6f73b8249a953a5dd2b1b3387786f926f1
Ramisettisanthoshi <143481554+Ramisettisanthoshi@users.noreply.github.com>
2023-09-07 09:53:08 +0530

    import java.util.*; public class hii {     public static void main(String args[])     {         int n;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         if(n%2==0)         {             System.out.println("Even");         }         else{             System.out.println("Odd");         }
        }
    }

---
+++ Even_or_Odd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class hii
+{
+    public static void main(String args[])
+    {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        if(n%2==0)
+        {
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+        
+    }
+}
