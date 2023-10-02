7d71c8ee36a585d0b15c1745a69d01bdeb97f223
Ramisettisanthoshi <143481554+Ramisettisanthoshi@users.noreply.github.com>
2023-10-02 20:55:11 +0530

    import java.util.*; public class hii {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int inches=sc.nextInt();         double centimeters=inches*2.54;         System.out.printf("%.2f",centimeters);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class hii
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int inches=sc.nextInt();
+        double centimeters=inches*2.54;
+        System.out.printf("%.2f",centimeters);
+    }
+}
