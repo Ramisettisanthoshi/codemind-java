7092ac262076a862e9dc258a75b9b94252634d30
Ramisettisanthoshi <143481554+Ramisettisanthoshi@users.noreply.github.com>
2023-10-13 11:09:59 +0530

    /* package codechef; // don't place package name! */
    import java.util.Scanner;
    
    /* Name of the class has to be "Main" only if the class is public. */
     public class Codechef
    {
        public static void main (String[] args) 
        {
            Scanner sc= new Scanner(System.in);
                
                int x = sc.nextInt();
                int y = sc.nextInt();
                
                if (x == 0 && y % 2 == 0){
                    System.out.println("YES");
                }
                else if (x == 0 && y % 2 != 0){
                    System.out.println("NO");
                }
                else if ((x + (2 * y)) % 2 == 0){
                    System.out.println("YES");
                }
                else {
                    System.out.println("NO");
                }
        }
    }

---
+++ Can_share_equally?.java
@@ -0,0 +1,28 @@
+/* package codechef; // don't place package name! */
+
+import java.util.Scanner;
+
+/* Name of the class has to be "Main" only if the class is public. */
+ public class Codechef
+{
+    public static void main (String[] args) 
+    {
+        Scanner sc= new Scanner(System.in);
+            
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            
+            if (x == 0 && y % 2 == 0){
+                System.out.println("YES");
+            }
+            else if (x == 0 && y % 2 != 0){
+                System.out.println("NO");
+            }
+            else if ((x + (2 * y)) % 2 == 0){
+                System.out.println("YES");
+            }
+            else {
+                System.out.println("NO");
+            }
+    }
+}
